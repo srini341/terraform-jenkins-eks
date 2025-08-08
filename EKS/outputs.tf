3a872bc90d11016cb0603af345b6a1fd59992049
srini341 <84306544+srini341@users.noreply.github.com>
2025-08-08 22:03:14 +0530

    Update outputs.tf

--- EKS/outputs.tf
+++ EKS/outputs.tf
@@ -7,8 +7,3 @@ output "cluster_name" {
   description = "The name of the EKS cluster"
   value       = module.eks.cluster_name
 }
-
-output "config_map_aws_auth" {
-  description = "Kubernetes ConfigMap for AWS IAM authentication"
-  value       = module.eks.config_map_aws_auth
-}
