5c4df9f6c48dd73fdbb9bc24e136b22e9d0c8c5e
srini341 <84306544+srini341@users.noreply.github.com>
2025-08-13 09:46:05 +0530

    Update outputs.tf

--- EKS/outputs.tf
+++ EKS/outputs.tf
@@ -1,9 +1,22 @@
-output "cluster_endpoint" {
-  description = "The endpoint for the EKS cluster"
-  value       = module.eks.cluster_endpoint
+output "eks_cluster_name" {
+  value       = module.eks.cluster_id
+  description = "EKS cluster name"
 }
 
-output "cluster_name" {
-  description = "The name of the EKS cluster"
-  value       = module.eks.cluster_name
+output "eks_cluster_endpoint" {
+  value       = module.eks.cluster_endpoint
+  description = "EKS cluster endpoint"
+}
+
+output "eks_cluster_ca" {
+  value       = module.eks.cluster_certificate_authority_data
+  description = "EKS cluster CA certificate"
+}
+
+output "node_group_role_arn" {
+  value       = module.eks.eks_managed_node_groups["default"].iam_role_arn
+  description = "IAM role ARN for the default node group"
+}
+output "eks_debug" {
+  value = module.eks
 }
